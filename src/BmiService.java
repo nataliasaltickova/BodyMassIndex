0223503d1ec25ead11c88f25e0bed721ed0286dc
nataliasaltickova <nataliasaltickova@yandex.ru>
2022-03-30 21:32:51 +0400

    Addressed comments

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,5 @@
 public class BmiService {
     public double calculate(double mass, double height) {
-        double bmi = mass / Math.pow(height,2);
-        return bmi;
+        return mass / Math.pow(height,2);
     }
-
 }
